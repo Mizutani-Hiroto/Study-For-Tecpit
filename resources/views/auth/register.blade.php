52b701803ff2bd8fe9d4cdd60f9fb696ad87fb5d
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2021-03-20 12:53:51 +0000

    ＶFＳコネクトエラー回避探索

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,10 +3,10 @@
 @section('title','ユーザー登録')
 
 @section('content')
-  <div class="contentainer">
+  <div class="container">
       <div class="row">
           <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg7 col-xl-6">
-              <h1 class="text-center"><a class="text-dark" href="/"></a>memo</h1>
+              <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
               <div class="card mt-3">
                   <div class="card-body text-center">
                       <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
