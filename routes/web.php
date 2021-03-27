063e608e2713328022ea2d724633c8982785fc52
Mizutani-Hiroto <mztnhrt0428@gmail.com>
2021-03-27 13:01:43 +0000

    onlyメソッドを使用

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ if(env('APP_ENV') === 'local') {
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->except(['index','show'])->middleware('auth');
+Route::resource('/articles','ArticleController')->only(['show']);
